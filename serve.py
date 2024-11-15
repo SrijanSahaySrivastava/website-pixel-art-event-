9df3bb6e8412b297ebfe0a08fa811d015657deb2
Srijan Sahay Srivastava <70461251+SrijanSahaySrivastava@users.noreply.github.com>
2024-11-15 18:40:26 +0530

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,10 +1,20 @@
 import http.server
 import socketserver
+import signal
+import sys
 
 PORT = 3000
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    def signal_handler(sig, frame):
+        print('Shutting down the server...')
+        httpd.shutdown()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
